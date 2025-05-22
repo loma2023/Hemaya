35a7c8dee06469d5fab0716b6dbc9e5ded85c0c4
eslam <121362570+loma2023@users.noreply.github.com>
2025-05-22 21:34:35 +0300

    Update 20-Collects.js

--- public/Js/20-Collects.js
+++ public/Js/20-Collects.js
@@ -26,7 +26,7 @@ function ShowAllReceipts(MainData, Decode) {
 
             let ChooseStatus = Status1
             let Name = (Customers.find(item => item._id == Receipt.Name))
-            if (Name === undefined) { ChooseStatus = Status2; Name = (Revenues.find(item => item._id == Receipt.Name)) }
+            if (Name === undefined) { ChooseStatus = Status2; Name = "123" }
             
             if (Receipt.DocType.includes("قبض")) { SumDebit += Receipt.Total }
             if (Receipt.DocType.includes("دفع")) { SumCredit += Receipt.Total }
