bf28c5c3ad25d0a22534fc35c875ded91a9019ee
eslam <121362570+loma2023@users.noreply.github.com>
2025-05-31 22:50:26 +0300

    Update 20-Collects.js

--- public/Js/20-Collects.js
+++ public/Js/20-Collects.js
@@ -154,7 +154,8 @@ function EditCollectOrPayment(id) {
 
     if (Total.value === "" || Total.value < 0.1) { Total.classList.add("Required"); return Toast(id = "Notification", txt = "يرجي إدخال المبلغ ",); }
     if (DocDate.value === "") { DocDate.classList.add("Required"); return Toast(id = "Notification", txt = "يرجي إدخال التاريخ ",); }
-
+if (CustomerID === ""){return Toast(id = "Notification", txt = "حاول مره اخري ",); }
+    
     let DocType = "سند قبض", Debit = "الصندوق",
         Credit = Status, SubDebit = "مقبوضات", SubCredit = CustomerID
 
